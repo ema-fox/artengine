d9e8a3e6d1d60dd7ba48b1dc270e6547dc24409e
Emanuel Rylke <ema-fox@web.de>
2012-03-31 11:55:32 +0200

    autosmooth

--- src/artengine/polygon.clj
+++ src/artengine/polygon.clj
@@ -1,6 +1,6 @@
 (ns artengine.polygon
   (:use [artengine util edit])
-  (:import [java.awt.geom Area]))
+  (:import [java.awt.geom Area GeneralPath]))
 
 (defn scramble [x]
   (mod (* 111 (+ 5 (bit-xor x (bit-shift-right x 2)))) 64))
@@ -42,12 +42,37 @@
   (doto (Area. pol2)
     (.intersect (Area. pol1))))
 
+(defn move-to [path [p0 p1]]
+  (.moveTo path p0 p1))
+
+(defn quad-to [path [pa0 pa1] [pb0 pb1]]
+  (.quadTo path pa0 pa1 pb0 pb1))
+
+(defn line-to [path [p0 p1]]
+  (.lineTo path p0 p1))
+
+(defn make-path [ps]
+  (let [path (GeneralPath.)]
+    (when (second ps)
+      (move-to path (avg-point (first ps) (second ps) 0.5))
+      (dorun (map (fn [pa pb pc]
+		    (if (< (Math/abs (- (first (avec<-dvec (minus pa pb)))
+					(first (avec<-dvec (minus pb pc))))) 1)
+		      (quad-to path pb (avg-point pb pc 0.5))
+		      (do
+			(line-to path pb)
+			(line-to path (avg-point pb pc 0.5)))))
+		  ps
+		  (concat (rest ps) (take 1 ps))
+		  (concat (drop 2 ps) (take 2 ps)))))
+    path))
+
 (defn get-polygon [{:keys [ps ls decos clip] :as x} xs]
-  (let [pol (make-polygon (if decos
-			    (let [deco-objs (for [i decos] (get xs i))]
-			      (apply concat (for [[ia ib] (get-ilines x)]
-					      (decorate-line (get ps ia) (get ps ib) deco-objs (bit-xor ia ib)))))
-			    (map #(get ps %) ls)))]
+  (let [pol (make-path (if decos
+			 (let [deco-objs (for [i decos] (get xs i))]
+			   (apply concat (for [[ia ib] (get-ilines x)]
+					   (decorate-line (get ps ia) (get ps ib) deco-objs (bit-xor ia ib)))))
+			 (map #(get ps %) ls)))]
     (if clip
       (clip-polygon pol (get-polygon (get xs clip) xs))
       pol)))
