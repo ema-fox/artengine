4f3e8c9167670571f47ae56cc3bb2b6a3c632692
Emanuel Rylke <ema-fox@web.de>
2014-11-14 16:33:37 +0100

    update jogl dependecy

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "vector drawing program"
   :main artengine.core
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [org.clojars.toxi/jogl "2.0.0-rc11"]
+                 [org.jogamp.gluegen/gluegen-rt-main "2.0-rc11"]
+                 [org.jogamp.jogl/jogl-all-main "2.0-rc11"]
 		 [seesaw "1.4.4"]])
